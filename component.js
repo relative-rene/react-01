1ef450f03060f3103cdc7c17e54433be33efdbb2
relative-rene <rene.arellano415@gmail.com>
2016-07-19 16:41:31 -0700

    Comment and CommentBox

--- component.js
+++ component.js
@@ -1,12 +1,31 @@
+class CommentBox extends React.Component {
+  render() {
+    return(
+      <div className="comment-box">
+        <h3>Comments</h3>
+        <h4 className="comment-count">2 comments</h4>
+        <div className="comment-list">
+          <Comment author="Anne Droid" body="I want to know what love is..."/>
+        </div>
+      </div>
+    );
+  }
+}
+
 class Comment extends React.Component {
   render() {
-    return
-    <div className="comment">
-      <p className="comment-header">Anne Droid</p>
-      <p className="comment-body">  I wanna know what love is...</p>
-      <div className="comment-actions">
-        <a href="#">Delete comment</a>
+    return(
+      <div className="comment">
+        <p className="comment-header">
+          {this.props.author}
+        </p>
+        <p className="comment-body">
+          {this.props.body}
+        </p>
+        <div className="comment-actions">
+          <a href="#">Delete comment</a>
+        </div>
       </div>
-    </div>
+    );
   }
 }
