3df75296cf05b0ace8114b643ea08588f28f8dd1
relative-rene <rene.arellano415@gmail.com>
2016-07-27 14:37:10 -0700

    _fetchComments ajax call

--- component.js
+++ component.js
@@ -1,3 +1,92 @@
+class CommentBox extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      showComments: false,
+      comments: []
+    };
+  }
+
+  render() {
+    const comments = this._getComments();
+    return(
+      <div className="comment-box">
+        <CommentForm addComment={this._addComment.bind(this)} />
+
+        {this._getPopularMessage(comments.length)}
+        <h3 className="comment-count">{this._getCommentsTitle(comments.length)}</h3>
+        <div className="comment-list">
+          {comments}
+        </div>
+      </div>
+    );
+  }
+
+  _getPopularMessage(commentCount) {
+    const POPULAR_COUNT = 10;
+    if (commentCount > POPULAR_COUNT) {
+       return (
+         <div>This post is getting really popular, don't miss out!</div>
+       );
+    }
+  }
+
+  _getComments() {
+    return this.state.comments.map((comment) => {
+      return (<Comment
+               id={comment.id}
+               author={comment.author}
+               body={comment.body}
+               avatarUrl={comment.avatarUrl}
+               key={comment.id} />);
+    });
+  }
+
+  _getCommentsTitle(commentCount) {
+    if (commentCount === 0) {
+      return 'No comments yet';
+    } else if (commentCount === 1) {
+      return '1 comment';
+    } else {
+      return `${commentCount} comments`;
+    }
+  }
+
+  _addComment(commentAuthor, commentBody) {
+    let comment = {
+      id: Math.floor(Math.random() * (9999 - this.state.comments.length + 1)) + this.state.comments.length,
+      author: commentAuthor,
+      body: commentBody,
+      avatarUrl: 'images/default-avatar.png'
+    };
+
+    this.setState({
+      comments: this.state.comments.concat([comment])
+    });
+  }
+
+  _deleteComment(commentID) {
+    const comments = this.state.comments.filter(
+      comment => comment.id !== commentID
+    );
+
+    this.setState({ comments });
+  }
+
+  _fetchComments() {
+    $.ajax({
+      method: 'GET',
+      url: 'comments.json',
+      success: (comments) => this.setState({comments})
+    });
+  }
+
+  componentWillMount() {
+    this._fetchComments();
+  }
+}
+
 class Comment extends React.Component {
   constructor() {
     super();
@@ -8,11 +97,13 @@ class Comment extends React.Component {
 
   render() {
     let commentBody;
+
     if (!this.state.isAbusive) {
       commentBody = this.props.body;
     } else {
       commentBody = <em>Content marked as abusive</em>;
     }
+
     return(
       <div className="comment">
         <img src={this.props.avatarUrl} alt={`${this.props.author}'s picture`} />
@@ -35,75 +126,13 @@ class Comment extends React.Component {
       isAbusive: !this.state.isAbusive
     });
   }
-}
 
-class CommentBox extends React.Component {
-  constructor() {
-    super();
+  _handleDelete(event) {
+    event.preventDefault();
 
-    this.state = {
-      showComments: false,
-      comments: [
-        { id: 1, author: 'Morgan McCircuit', body: 'Great picture!', avatarUrl: 'images/default-avatar.png' },
-        { id: 2, author: 'Bending Bender', body: 'Excellent stuff', avatarUrl: 'images/default-avatar.png' }
-      ]
-    };
-  }
-
-  render() {
-    const comments = this._getComments();
-    return(
-      <div className="comment-box">
-        <CommentForm addComment={this._addComment.bind(this)} />
-        <h3>Comments</h3>
-        {this._getPopularMessage(comments.length)}
-        <h4 className="comment-count">{this._getCommentsTitle(comments.length)}</h4>
-        <div className="comment-list">
-          {comments}
-        </div>
-      </div>
-    );
-  }
-
-  _getPopularMessage(commentCount) {
-    const POPULAR_COUNT = 10;
-    if (commentCount > POPULAR_COUNT) {
-       return (
-         <div>This post is getting really popular, don't miss out!</div>
-       );
+    if (confirm('Are you sure?')) {
+      this.props.onDelete(this.props.id);
     }
-  }
-
-  _getComments() {
-    return this.state.comments.map((comment) => {
-      return (<Comment
-               author={comment.author}
-               body={comment.body}
-               avatarUrl={comment.avatarUrl}
-               key={comment.id} />);
-    });
-  }
-
-  _getCommentsTitle(commentCount) {
-    if (commentCount === 0) {
-      return 'No comments yet';
-    } else if (commentCount === 1) {
-      return '1 comment';
-    } else {
-      return `${commentCount} comments`;
-    }
-  }
-
-  _addComment(commentAuthor, commentBody) {
-    let comment = {
-      id: Math.floor(Math.random() * (9999 - this.state.comments.length + 1)) + this.state.comments.length,
-      author: commentAuthor,
-      body: commentBody
-    };
-
-    this.setState({
-      comments: this.state.comments.concat([comment])
-    });
   }
 }
 
@@ -120,11 +149,8 @@ class CommentForm extends React.Component {
       <form className="comment-form" onSubmit={this._handleSubmit.bind(this)}>
         <label>New comment</label>
         <div className="comment-form-fields">
-          <input placeholder="Name:" ref={input => this._author = input} />
-          <textarea
-            placeholder="Comment:"
-            ref={textarea => this._body = textarea}
-            onKeyUp={this._getCharacterCount.bind(this)}></textarea>
+          <input placeholder="Name:" ref={c => this._author = c} />
+          <textarea placeholder="Comment:" ref={c => this._body = c} onChange={this._getCharacterCount.bind(this)}></textarea>
         </div>
         <p>{this.state.characters} characters</p>
         <div className="comment-form-actions">
@@ -136,23 +162,25 @@ class CommentForm extends React.Component {
     );
   }
 
+  _getCharacterCount(e) {
+    this.setState({
+      characters: this._body.value.length
+    });
+  }
+
   _handleSubmit(event) {
     event.preventDefault();
-    if(!this._author.value || !this._body.value) {
-      alert('Please enter your name and comment');
-      return
+
+    if (!this._author.value || !this._body.value) {
+      alert('Please enter your name and comment.');
+      return;
     }
+
     this.props.addComment(this._author.value, this._body.value);
 
     this._author.value = '';
     this._body.value = '';
 
-    this.setState({ characters: 0 });
-  }
-
-  _getCharacterCount() {
-    this.setState({
-      characters: this._body.value.length
-    });
+    this.setState({ characters: 0  });
   }
 }
