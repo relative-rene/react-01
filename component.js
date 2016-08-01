9a703a5d781a8dc9677f0ed19bf5879c1f3ee9b6
relative-rene <rene.arellano415@gmail.com>
2016-08-01 13:48:48 -0700

    components.js

--- component.js
+++ component.js
@@ -1,4 +1,5 @@
 class CommentBox extends React.Component {
+
   constructor() {
     super();
 
@@ -15,15 +16,22 @@ class CommentBox extends React.Component {
   render() {
     const comments = this._getComments();
     return(
-      <div className="comment-box">
-        <CommentForm addComment={this._addComment.bind(this)} />
-        <CommentAvatarList avatars={this._getAvatars()} />
-        {this._getPopularMessage(comments.length)}
-        <h3 className="comment-count">{this._getCommentsTitle(comments.length)}</h3>
-        <div className="comment-list">
-          {comments}
+      <div className="row comments-container">
+        <div className="cell">
+          <h2>Join The Discussion</h2>
+          <div className="comment-box">
+            <CommentForm addComment={this._addComment.bind(this)} />
+            <CommentAvatarList avatars={this._getAvatars()} />
+
+            {this._getPopularMessage(comments.length)}
+            <h3 className="comment-count">{this._getCommentsTitle(comments.length)}</h3>
+            <div className="comment-list">
+              {comments}
+            </div>
+          </div>
         </div>
       </div>
+
     );
   }
 
@@ -35,20 +43,20 @@ class CommentBox extends React.Component {
     const POPULAR_COUNT = 10;
     if (commentCount > POPULAR_COUNT) {
        return (
-         <div>This post is getting really popular, don't miss out!</div>
+         <div>This post is getting really popular, dont miss out!</div>
        );
     }
   }
 
   _getComments() {
     return this.state.comments.map((comment) => {
-      return (<Comment
+      return <Comment
                id={comment.id}
                author={comment.author}
                body={comment.body}
                avatarUrl={comment.avatarUrl}
                onDelete={this._deleteComment.bind(this)}
-               key={comment.id} />);
+               key={comment.id} />
     });
   }
 
@@ -63,24 +71,26 @@ class CommentBox extends React.Component {
   }
 
   _addComment(commentAuthor, commentBody) {
-    let comment = {
-      id: Math.floor(Math.random() * (9999 - this.state.comments.length + 1)) + this.state.comments.length,
+
+    const comment = {
+      id: this.state.comments.length + 1,
       author: commentAuthor,
       body: commentBody,
-      avatarUrl: 'images/default-avatar.png'
+      avatarUrl: 'assets/images/avatars/avatar-default.png'
     };
 
     this.setState({
       comments: this.state.comments.concat([comment])
     });
+
   }
 
   _fetchComments() {
-    $.ajax({
+    jQuery.ajax({
       method: 'GET',
       url: 'comments.json',
       success: (comments) => {
-        this.setState({ comments });
+        this.setState({ comments })
       }
     });
   }
@@ -91,55 +101,6 @@ class CommentBox extends React.Component {
     );
 
     this.setState({ comments });
-  }
-}
-
-class Comment extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      isAbusive: false
-    };
-  }
-
-  render() {
-    let commentBody;
-
-    if (!this.state.isAbusive) {
-      commentBody = this.props.body;
-    } else {
-      commentBody = <em>Content marked as abusive</em>;
-    }
-
-    return(
-      <div className="comment">
-        <img src={this.props.avatarUrl} alt={`${this.props.author}'s picture`} />
-        <p className="comment-header">{this.props.author}</p>
-        <p className="comment-body">
-          {commentBody}
-        </p>
-        <div className="comment-actions">
-          <a href="#">Delete comment</a>
-          <a href="#" onClick={this._toggleAbuse.bind(this)}>Report as Abuse</a>
-        </div>
-      </div>
-    );
-  }
-
-  _toggleAbuse(event) {
-    event.preventDefault();
-
-    this.setState({
-      isAbusive: !this.state.isAbusive
-    });
-  }
-
-  _handleDelete(event) {
-    event.preventDefault();
-
-    if (confirm('Are you sure?')) {
-      this.props.onDelete(this.props.id);
-    }
   }
 }
 
@@ -169,19 +130,16 @@ class CommentForm extends React.Component {
     );
   }
 
-  _getCharacterCount(e) {
+  _getCharacterCount() {
+
     this.setState({
       characters: this._body.value.length
     });
+
   }
 
   _handleSubmit(event) {
     event.preventDefault();
-
-    if (!this._author.value || !this._body.value) {
-      alert('Please enter your name and comment.');
-      return;
-    }
 
     this.props.addComment(this._author.value, this._body.value);
 
@@ -194,7 +152,9 @@ class CommentForm extends React.Component {
 
 class CommentAvatarList extends React.Component {
   render() {
+
     const { avatars = [] } = this.props;
+
     return (
       <div className="comment-avatars">
         <h4>Authors</h4>
@@ -206,6 +166,106 @@ class CommentAvatarList extends React.Component {
           ))}
         </ul>
       </div>
-    );
+    )
   }
 }
+
+class Comment extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isAbusive: false
+    };
+  }
+
+  render() {
+
+    let commentBody;
+
+    if (!this.state.isAbusive) {
+      commentBody = this.props.body;
+    } else {
+      commentBody = <em>Content marked as abusive</em>;
+    }
+
+    return(
+      <div className="comment">
+
+        <img src={this.props.avatarUrl} alt={`${this.props.author}'s picture`} />
+
+        <p className="comment-header">{this.props.author}</p>
+        <p className="comment-body">{commentBody}</p>
+
+        <div className="comment-actions">
+          <CommentRemoveConfirmation onDelete={this._handleDelete.bind(this)} />
+          <a href="#" onClick={this._toggleAbuse.bind(this)}>Report as Abuse</a>
+        </div>
+      </div>
+    );
+  }
+
+  _toggleAbuse(event) {
+    event.preventDefault();
+
+    this.setState({
+      isAbusive: !this.state.isAbusive
+    });
+  }
+
+  _handleDelete() {
+    this.props.onDelete(this.props.id);
+  }
+}
+
+class CommentRemoveConfirmation extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      showConfirm: false
+    };
+  }
+
+  render() {
+
+    let confirmNode;
+
+    if (this.state.showConfirm) {
+      return (
+        <span>
+          <a href="" onClick={this._confirmDelete.bind(this)}>Yes </a> - or - <a href="" onClick={this._toggleConfirmMessage.bind(this)}> No</a>
+        </span>
+      );
+    } else {
+      confirmNode = <a href="" onClick={this._toggleConfirmMessage.bind(this)}>Delete comment?</a>;
+    }
+
+    return (
+      <span>
+        {confirmNode}
+      </span>
+    );
+  }
+
+  _toggleConfirmMessage(e) {
+    e.preventDefault();
+
+    this.setState({
+      showConfirm: !this.state.showConfirm
+    });
+
+  }
+
+  _confirmDelete(e) {
+    e.preventDefault();
+    this.props.onDelete();
+  }
+}
+
+jQuery(function() {
+  ReactDOM.render(
+    <CommentBox />,
+    document.getElementById('comment-box')
+  );
+})
